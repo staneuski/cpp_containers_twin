bf52ff9d0bdcc1df0d3e7635a462f5c6a6509818
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2021-04-12 00:21:34 +0300

    Create simple_vector/array_ptr.h – a smart pointer to an array in heap
    https://praktikum.yandex.ru/trainer/cpp/lesson/430d7fe3-4b07-4fb9-9bba-904427ae3358/task/1ab22c0b-8a78-49ef-bbcd-987a3b2b8842/

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <cassert>
+#include <iostream>
 #include <stdexcept>
 
-#include "single_linked_list/single_linked_list.h"
+#include "simple_vector/array_ptr.h"
 
 int main() {
-    SingleLinkedList<int> numbers{0, 1, 2, 3};
-    std::cout << "numbers: " << numbers << std::endl;
+    using namespace std;
 
-    SingleLinkedList<std::string> words ={"single", "linked", "list"};
-    std::cout << "words: " << words << std::endl;
+    ArrayPtr<int> numbers(10);
+    const auto& const_numbers = numbers;
 
-    return 0;
+    numbers[2] = 42;
+
+    assert(numbers);
+    assert(const_numbers[2] == 42);
+    assert(&const_numbers[2] == &numbers[2]);
+
+    assert(numbers.Get() == &numbers[0]);
+
+    ArrayPtr<int> numbers_2(5);
+    numbers_2[2] = 43;
+
+    numbers.swap(numbers_2);
+
+    assert(numbers_2[2] == 42);
+    assert(numbers[2] == 43);
+
+    cout << "[ ";
+    for (size_t i = 0u; i < 10u; ++i)
+        cout << numbers[i] << ' ';
+    cout << ']' << endl;
 }
---
+++ src/simple_vector/array_ptr.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <cassert>
+#include <cstdlib>
+#include <utility>
+
+template <typename Type>
+class ArrayPtr {
+public:
+    ArrayPtr() = default;
+
+    // Создаёт в куче массив из size элементов типа Type.
+    // Если size == 0, поле raw_ptr_ должно быть равно nullptr
+    explicit ArrayPtr(size_t size)
+        : raw_ptr_(size ? new Type[size] : nullptr)
+    {
+    }
+
+    // Конструктор из сырого указателя, хранящего адрес массива в куче либо nullptr
+    explicit ArrayPtr(Type* raw_ptr) noexcept
+        : raw_ptr_(raw_ptr)
+    {
+    }
+
+    // Запрещаем копирование
+    ArrayPtr(const ArrayPtr&) = delete;
+
+    ~ArrayPtr() {
+        delete[] raw_ptr_;
+    }
+
+    // Запрещаем присваивание
+    ArrayPtr& operator=(const ArrayPtr&) = delete;
+
+    // Прекращает владением массивом в памяти, возвращает значение адреса массива
+    // После вызова метода указатель на массив должен обнулиться
+    [[nodiscard]] Type* Release() noexcept {
+        Type* raw_ptr = raw_ptr_;
+        raw_ptr_ = nullptr;
+        return raw_ptr;
+    }
+
+    // Возвращает ссылку на элемент массива с индексом index
+    inline Type& operator[](size_t index) noexcept {
+        return *(raw_ptr_ + index);
+    }
+
+    // Возвращает константную ссылку на элемент массива с индексом index
+    inline const Type& operator[](size_t index) const noexcept {
+        return *(raw_ptr_ + index);
+    }
+
+    // Возвращает true, если указатель ненулевой, и false в противном случае
+    inline explicit operator bool() const {
+        return raw_ptr_;
+    }
+
+    // Возвращает значение сырого указателя, хранящего адрес начала массива
+    inline Type* Get() const noexcept {
+        return raw_ptr_;
+    }
+
+    // Обменивается значениям указателя на массив с объектом other
+    inline void swap(ArrayPtr& other) noexcept {
+        std::swap(raw_ptr_, other.raw_ptr_);
+    }
+
+private:
+    Type* raw_ptr_ = nullptr;
+};
